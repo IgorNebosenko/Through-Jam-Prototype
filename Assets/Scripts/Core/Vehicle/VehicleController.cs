4cf44977aeacc95b6f6da086f4a7c53dd26c1a30
igor_nebosenko <igor.nebosenko@gmail.com>
2023-04-19 06:30:48 +0300

    - Add vehicle controller

---
+++ Assets/Scripts/Core/Vehicle/VehicleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using ElectrumGames.Core.Vehicle.Movement;
+using UnityEngine;
+
+namespace ElectrumGames.Core.Vehicle
+{
+    public class VehicleController : MonoBehaviour
+    {
+        [SerializeField] private GuideWheel[] guideWheelsGroup;
+        [SerializeField] private MotorWheel[] motorWheelsGroup;
+        [SerializeField] private MotorWithGuideWheel[] motorWithGuideWheelsGroup;
+
+        private List<ICanRotate> _rotateWheels;
+        private List<IHaveMotor> _motorWheels;
+
+        private void Awake()
+        {
+            _rotateWheels = new List<ICanRotate>(guideWheelsGroup);
+            _rotateWheels.AddRange(motorWithGuideWheelsGroup);
+
+            _motorWheels = new List<IHaveMotor>(motorWheelsGroup);
+            _motorWheels.AddRange(motorWithGuideWheelsGroup);
+        }
+    }
+}
